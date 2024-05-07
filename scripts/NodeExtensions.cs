a1446cd75970b03a237988cfc7a67ee5f07b6ceb
Dejan Boras <de.jean.7777@protonmail.com>
2024-05-07 22:10:12 +0200

    Method to find child of type recursively in NodeExtensions

--- scripts/NodeExtensions.cs
+++ scripts/NodeExtensions.cs
@@ -6,13 +6,30 @@ public static class NodeExtensions
 {
     public static T FindChildOfType<T>(this Node node) where T : Node
     {
-        T child = default(T);
-
         foreach (var current in node.GetChildren())
         {
             if (current is T) return (T)current;
         }
 
-        return child;
+        return default;
+    }
+
+    public static T RecursiveFindChildOfType<T>(this Node node) where T : Node
+    {
+        foreach (var current in node.GetChildren())
+        {
+            if (current is T) return (T)current;
+
+        }
+
+        foreach (var current in node.GetChildren())
+        {
+            T child = RecursiveFindChildOfType<T>(current);
+
+            if (child != default(T)) return child;
+
+        }
+
+        return default;
     }
 }
